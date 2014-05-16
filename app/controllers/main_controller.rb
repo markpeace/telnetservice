8281bc898dc5652e8fcdcd0084a51dca2968c14c
markpeace <mark@learnsomestuff.com>
2014-05-16 09:38:55 +0000

    fixed header permission problem

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -21,6 +21,11 @@ class MainController < ApplicationController
 
                 host.close
 
+                headers['Access-Control-Allow-Origin'] = '*'
+                headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+                headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(',')
+                headers['Access-Control-Max-Age'] = "1728000"                
+
                 render :json => r
 
         end
