026d0b252a77a5b244b9243a2114d7c5cf2b94bc
markpeace <mark@learnsomestuff.com>
2014-12-29 17:25:43 +0000

    messing with sketch download

--- app/controllers/upload2arduino_controller.rb
+++ app/controllers/upload2arduino_controller.rb
@@ -17,8 +17,8 @@ class Upload2arduinoController < ApplicationController
                 require 'net/ssh'
 
                 Net::SSH.start('bowerfold.dlinkddns.com', 'root', :port=>150, :password => "mmmbeer00") do |ssh|
-                        ssh.exec "cd /"
-                        @m=ssh.exec!("ls")
+                        ssh.exec "touch mark.test"
+                        ssh.exec!("wget http://ordeal-dromic.codio.io:3000/sketch.hex")
                 end
                 
                 render :upload
