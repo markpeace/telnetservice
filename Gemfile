4fcac1367545d708637cf08989bf78401a11783a
markpeace <mark@learnsomestuff.com>
2014-12-29 16:38:34 +0000

    added ssh

--- Gemfile
+++ Gemfile
@@ -25,3 +25,5 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
 gem 'pushmeup'
+
+gem 'net-ssh'
--- app/controllers/upload2arduino_controller.rb
+++ app/controllers/upload2arduino_controller.rb
@@ -11,7 +11,17 @@ class Upload2arduinoController < ApplicationController
                         file.write(params[:sketch].read)
                 end
 
-                render :upload
-        end
 
+                require 'rubygems'
+                require 'net/http'
+                require 'net/ssh'
+
+                Net::SSH.start('bowerfold.dlinkddns.com', 'root', :port=>150, :password => "mmmbeer00") do |ssh|
+                        ssh.exec "cd /"
+                        @m=ssh.exec!("ls")
+                end
+                
+                render :upload
+
+        end
 end
